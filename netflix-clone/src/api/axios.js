008fe512273626e16e184d3db4514eafc884e518
salmonellatyphii22 <swetajha462@gmail.com>
2025-12-30 14:38:20 +0530

    Fix: remove nested git repo

---
+++ netflix-clone/src/api/axios.js
@@ -0,0 +1,7 @@
+import axios from "axious";
+
+const instance=axios.create({
+    baseURL:"https://api.themoviedb.org/3",
+});
+
+export default instance;
---
+++ netflix-clone/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import react from "react";
+import "./Navbar.css";
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <h2 className="logo">Netflix</h2>
+        </div>
+    );
+}
