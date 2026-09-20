a7ad4bd68150f1b22dcd450a5c81f628ee3c6fc5
salmonellatyphii22 <swetajha462@gmail.com>
2026-09-20 16:05:15 +0530

    Fix backend CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         
-        "https://bingebox-f3t3.onrender.com/"
+        "https://bingeboxnetc.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
